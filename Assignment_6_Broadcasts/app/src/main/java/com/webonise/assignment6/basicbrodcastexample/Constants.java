4217041ff9d633f09cf62f050b7e852a64d127f0
shubhamdani <shubhamdani-weboniselab>
2015-08-11 20:10:16 +0530

    compleated assignment 6

---
+++ Assignment_6_Broadcasts/app/src/main/java/com/webonise/assignment6/basicbrodcastexample/Constants.java
@@ -0,0 +1,10 @@
+package com.webonise.assignment6.basicbrodcastexample;
+
+/**
+ * Created by webonise on 11/8/15.
+ */
+public class Constants  {
+    public static final String SMS_BUNDLE = "pdus";
+    public static final String SENDER = "The Sender is: ";
+    public static final String MSG = "Message is ";
+}
